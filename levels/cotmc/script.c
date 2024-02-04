cbca0402eb0c74cbe7cee009b79bd7bf27eed882
ManIsCat2 <137772623+ManIsCat2@users.noreply.github.com>
2024-02-04 07:40:57 -0800

    fix me being dumb

--- levels/cotmc/script.c
+++ levels/cotmc/script.c
@@ -93,7 +93,7 @@ const LevelScript level_cotmc_entry[] = {
 	WARP_NODE(10, 26, 1, 1, 0),
 	WARP_NODE(240, 6, 1, 50, 0),
 	WARP_NODE(241, 6, 1, 100, 0),
-	OBJECT_WITH_ACTS(E_MODEL_TOAD, -246, 130, 49, 0, 270, 0, 0x4000203, id_bhvToadMessage, 31),
+	OBJECT_WITH_ACTS(E_MODEL_TOAD, -246, 130, 49, 0, 270, 0, 0x1, id_bhvToadMessage, 31),
 	OBJECT_WITH_ACTS(0, -1638, 0, 98, 0, 90, 0, 0xa0000, bhvSpinAirborneWarp, 31),
 	OBJECT_WITH_ACTS(0, -2142, 0, 62, 0, 90, 0, 0xa0a0000, bhvWarp, 31),
 	// OBJECT_WITH_ACTS(0, 0, 0, 0, 0, 0, 0, 0x0, 0x0041f5a0, 31),
