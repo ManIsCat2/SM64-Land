0c25c3a9eebc7342439f5c71155e0ae850d7a035
ManIsCat2 <137772623+ManIsCat2@users.noreply.github.com>
2024-02-04 08:19:25 -0800

    make cat dive a custom action

--- catsuit.lua
+++ catsuit.lua
@@ -4,6 +4,9 @@ catsuit = false
 E_MODEL_CAT_MARIO = smlua_model_util_get_id("cat_mario_geo")
 E_MDOEL_SUPER_BELL = smlua_model_util_get_id("super_bell_geo")
 
+ACT_CAT_DIVE = allocate_mario_action(ACT_GROUP_AIRBORNE | ACT_FLAG_AIR | ACT_FLAG_MOVING |
+    ACT_FLAG_ALLOW_VERTICAL_WIND_ACTION)
+
 ---@param obj Object
 function bhv_catsuit_init(obj)
     obj.oFlags = OBJ_FLAG_UPDATE_GFX_POS_AND_ANGLE
@@ -43,12 +46,32 @@ function catsuit_power_loop(m)
     if m.playerIndex == 0 then
         if catsuit then
             if m.controller.buttonDown & Y_BUTTON ~= 0 and m.action & ACT_FLAG_AIR ~= 0 then
-                set_mario_animation(m, MARIO_ANIM_DIVE)
-                m.forwardVel = 40
-                m.vel.y = -9
+                set_mario_action(m, ACT_CAT_DIVE, 0)
             end
         end
     end
+end
+
+function cat_dive(m)
+    update_walking_speed(m)
+    perform_air_step(m, 0)
+    m.actionTimer = m.actionTimer + 1
+    perform_air_step(m, 0)
+    set_mario_animation(m, MARIO_ANIM_DIVE)
+    m.forwardVel = 20
+    if m.controller.buttonDown & Y_BUTTON == 0 then
+        return set_mario_action(m, ACT_FREEFALL, 0)
+    end
+
+    if perform_air_step(m, 0) == AIR_STEP_LANDED then
+        set_mario_action(m, ACT_FREEFALL_LAND, 0)
+    end
+
+    return 0
+end
+
+function cat_dive_gravity(m)
+    m.vel.y = m.vel.y - 4.5
 end
 
 function models(m)
@@ -59,3 +82,4 @@ end
 
 hook_event(HOOK_MARIO_UPDATE, models)
 hook_event(HOOK_MARIO_UPDATE, catsuit_power_loop)
+hook_mario_action(ACT_CAT_DIVE, { every_frame = cat_dive, gravity = cat_dive_gravity }, INT_FAST_ATTACK_OR_SHELL)
