1fe1fba70e0a0252b6baf9fd1009509d269ea621
ManIsCat2 <137772623+ManIsCat2@users.noreply.github.com>
2024-02-06 03:22:00 -0800

    add jrb (still wip)

---
+++ custom_surface_types.lua
@@ -0,0 +1,13 @@
+SURFACE_BOUNCY_CUSTOM = 0x2
+
+
+function surface_check(m)
+    if m.playerIndex ~= 0 then return end
+
+    if m.floor and m.floor.type == SURFACE_BOUNCY_CUSTOM and m.floorHeight == m.pos.y then
+        set_mario_action(m, ACT_DOUBLE_JUMP, 0)
+        m.vel.y = 130
+    end
+end
+
+hook_event(HOOK_MARIO_UPDATE, surface_check)
---
+++ levels/jrb/script.c
@@ -0,0 +1,55 @@
+#include <ultra64.h>
+#include "sm64.h"
+#include "behavior_data.h"
+#include "model_ids.h"
+#include "seq_ids.h"
+#include "dialog_ids.h"
+#include "segment_symbols.h"
+#include "level_commands.h"
+
+#include "game/level_update.h"
+
+#include "levels/scripts.h"
+
+
+/* Fast64 begin persistent block [includes] */
+/* Fast64 end persistent block [includes] */
+
+#include "make_const_nonconst.h"
+#include "levels/jrb/header.h"
+
+/* Fast64 begin persistent block [scripts] */
+/* Fast64 end persistent block [scripts] */
+
+const LevelScript level_jrb_entry[] = {
+	INIT_LEVEL(),
+	LOAD_MIO0(0x7, _jrb_segment_7SegmentRomStart, _jrb_segment_7SegmentRomEnd), 
+	LOAD_MIO0(0xa, _water_skybox_mio0SegmentRomStart, _water_skybox_mio0SegmentRomEnd), 
+	ALLOC_LEVEL_POOL(),
+	MARIO(MODEL_MARIO, 0x00000001, bhvMario),
+
+	/* Fast64 begin persistent block [level commands] */
+	/* Fast64 end persistent block [level commands] */
+
+	AREA(1, jrb_area_1),
+		WARP_NODE(0x0A, LEVEL_BOB, 0x01, 0, WARP_NO_CHECKPOINT),
+		WARP_NODE(0xF0, 26, 1, 81, WARP_NO_CHECKPOINT),
+		WARP_NODE(0xF1, 26, 1, 91, WARP_NO_CHECKPOINT),
+		MARIO_POS(0x01, 0, -14044, -3931, -7371),
+		OBJECT(MODEL_NONE, -14044, -3931, -7371, 0, 90, 0, 0x000A0000, bhvSpinAirborneWarp),
+		TERRAIN(jrb_area_1_collision),
+		MACRO_OBJECTS(jrb_area_1_macro_objs),
+		SET_BACKGROUND_MUSIC(0x00, SEQ_LEVEL_GRASS),
+		TERRAIN_TYPE(TERRAIN_GRASS),
+		/* Fast64 begin persistent block [area commands] */
+		/* Fast64 end persistent block [area commands] */
+	END_AREA(),
+
+	FREE_LEVEL_POOL(),
+	MARIO_POS(0x01, 0, -14044, -3931, -7371),
+	CALL(0, lvl_init_or_update),
+	CALL_LOOP(1, lvl_init_or_update),
+	CLEAR_LEVEL(),
+	SLEEP_BEFORE_EXIT(1),
+	EXIT(),
+};
