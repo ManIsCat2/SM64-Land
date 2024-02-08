513d60a8ba2acabcc55a4a578979fb9f599083b6
xLuigiGamerx <mohammmedsadawi666@gmail.com>
2024-02-08 22:47:13 +0300

    Updated tanooki leaf with model ripped from rom hack

--- actors/tanooki_leaf/geo_header.h
+++ actors/tanooki_leaf/geo_header.h
@@ -1,7 +1,8 @@
 extern const GeoLayout tanooki_leaf_geo[];
-extern Lights1 tanooki_leaf_mat_leaf_f3d_lights;
-extern u8 tanooki_leaf_I_leaf_0_rgba16[];
-extern Vtx tanooki_leaf_Bone_mesh_layer_1_vtx_0[487];
+extern Lights1 tanooki_leaf__105_5F73EFCA_c_bmp_f3d_lights;
+extern u8 tanooki_leaf__5F73EFCA_c_rgba16[];
+extern Vtx tanooki_leaf_Bone_mesh_layer_1_vtx_0[132];
 extern Gfx tanooki_leaf_Bone_mesh_layer_1_tri_0[];
-extern Gfx mat_tanooki_leaf_mat_leaf_f3d[];
+extern Gfx mat_tanooki_leaf__105_5F73EFCA_c_bmp_f3d[];
 extern Gfx tanooki_leaf_Bone_mesh_layer_1[];
+extern Gfx tanooki_leaf_material_revert_render_settings[];
