7502e7fe321db048c1e318ccced4c10dd6c070b3
xLuigiGamerx <mohammmedsadawi666@gmail.com>
2024-02-11 20:51:41 +0300

    Increased the gravity

--- level-exclusives.lua
+++ level-exclusives.lua
@@ -1,6 +1,6 @@
 function level_1_3(m)
     if m.action & ACT_FLAG_AIR ~= 0 and gNetworkPlayers[0].currLevelNum == LEVEL_WF and gNetworkPlayers[0].currAreaIndex == 2 then
-        m.vel.y = m.vel.y + 1.3
+        m.vel.y = m.vel.y + 1.2
     end
 end
 
