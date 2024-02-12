1fa02153a411c1f7745fc16c77db9b473b188e54
xLuigiGamerx <mohammmedsadawi666@gmail.com>
2024-02-12 18:29:51 +0300

    Updated world 1's boss's level collision to use the one exported from rom manager

--- levels/bob/area_3/geo.inc.c
+++ levels/bob/area_3/geo.inc.c
@@ -7,10 +7,6 @@ const GeoLayout bob_area_3_geo[] = {
 		GEO_OPEN_NODE(),
 			GEO_DISPLAY_LIST(LAYER_ALPHA, bob_dl_Arena_mesh_layer_4),
 		GEO_CLOSE_NODE(),
-		GEO_ROTATION_NODE_WITH_DL(LAYER_OPAQUE, 90, 0, 0, bob_dl_Collisionless_Arena_Models_mesh_layer_1),
-		GEO_OPEN_NODE(),
-			GEO_DISPLAY_LIST(LAYER_ALPHA, bob_dl_Collisionless_Arena_Models_mesh_layer_4),
-		GEO_CLOSE_NODE(),
 	GEO_CLOSE_NODE(),
 	GEO_RETURN(),
 };
