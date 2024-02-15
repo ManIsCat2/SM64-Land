f261e92150e81a1211cf354bbbe31a6fc5d20e31
ManIsCat2 <137772623+ManIsCat2@users.noreply.github.com>
2024-02-14 23:48:06 -0800

    add star recolors (World 2)

---
+++ recolored_stars.lua
@@ -0,0 +1,11 @@
+-- make stars do color
+
+E_MODEL_STAR2 = smlua_model_util_get_id("star2_geo")
+
+function bhv_star_loop(o)
+    if gNetworkPlayers[0].currLevelNum == LEVEL_CCM or gNetworkPlayers[0].currLevelNum == LEVEL_JRB then
+        obj_set_model_extended(o, E_MODEL_STAR2)
+    end
+end
+
+hook_behavior(id_bhvStar, OBJ_LIST_GENACTOR, false, nil, bhv_star_loop)
