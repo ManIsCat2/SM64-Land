74f1a4a46d45bb33a526bfa002383379417db231
ManIsCat2 <137772623+ManIsCat2@users.noreply.github.com>
2024-03-22 01:44:27 +0330

    one more level

---
+++ levels/rr/areas/2/movtextNew.inc.c
@@ -0,0 +1,30 @@
+static Movtex rr_2_Movtex_0_1[] = {1, 0, 15, 16, -25000, -770, -9638, -770, -9638, 11997, -25000, 11997, 1, 79, 0, 0};
+
+static Movtex rr_2_Movtex_1_1[] = {1, 0, 15, 16, -25000, -770, -9638, -770, -9638, 11997, -25000, 11997, 1, 79, 0, 0};
+
+static Movtex rr_2_Movtex_2_1[] = {1, 0, 15, 16, -7381, 29071, 5876, 29071, 5876, 32735, -7381, 32735, 1, 79, 0, 0};
+
+static Movtex rr_2_Movtex_0_2[] = {1, 0, 15, 16, -22000, -32767, 32767, -32767, 32767, 32767, -22000, 32767, 1, 79, 0, 0};
+
+static Movtex rr_2_Movtex_1_2[] = {1, 0, 15, 16, -25000, -770, -9638, -770, -9638, 11997, -25000, 11997, 1, 79, 0, 0};
+
+static Movtex rr_2_Movtex_2_2[] = {1, 0, 15, 16, -7381, 29071, 5876, 29071, 5876, 32735, -7381, 32735, 1, 79, 0, 0};
+
+static Movtex rr_2_Movtex_3_2[] = {1, 0, 15, 16, -21576, -770, -9638, -770, -9638, 11997, -21576, 11997, 1, 79, 0, 0};
+
+const struct MovtexQuadCollection rr_2_Movtex_0[] = {
+{-1, NULL},
+};
+const struct MovtexQuadCollection rr_2_Movtex_1[] = {
+{0,rr_2_Movtex_0_1},
+{1,rr_2_Movtex_1_1},
+{2,rr_2_Movtex_2_1},
+{-1, NULL},
+};
+const struct MovtexQuadCollection rr_2_Movtex_2[] = {
+{0,rr_2_Movtex_0_2},
+{1,rr_2_Movtex_1_2},
+{2,rr_2_Movtex_2_2},
+{3,rr_2_Movtex_3_2},
+{-1, NULL},
+};
---
+++ levels/rr/texture.inc.c
@@ -0,0 +1,198 @@
+ALIGNED8 u8 rr_1__texture_0E000018[] = {
+#include "levels/rr/rr_1_0xe000018_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_1__texture_0E003020[] = {
+#include "levels/rr/rr_1_0xe003020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_1__texture_0E003820[] = {
+#include "levels/rr/rr_1_0xe003820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_1__texture_0E007040[] = {
+#include "levels/rr/rr_1_0xe007040_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_1__texture_0E008040[] = {
+#include "levels/rr/rr_1_0xe008040_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_1__texture_0E001820[] = {
+#include "levels/rr/rr_1_0xe001820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_1__texture_0E004020[] = {
+#include "levels/rr/rr_1_0xe004020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_1__texture_0E007840[] = {
+#include "levels/rr/rr_1_0xe007840_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_1__texture_0E002020[] = {
+#include "levels/rr/rr_1_0xe002020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_1__texture_0E005820[] = {
+#include "levels/rr/rr_1_0xe005820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_1__texture_0E00B840[] = {
+#include "levels/rr/rr_1_0xe00b840_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_1__texture_0E00C040[] = {
+#include "levels/rr/rr_1_0xe00c040_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_1__texture_0E006020[] = {
+#include "levels/rr/rr_1_0xe006020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_1__texture_0E00C840[] = {
+#include "levels/rr/rr_1_0xe00c840_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_1__texture_0E00D840[] = {
+#include "levels/rr/rr_1_0xe00d840_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_1__texture_0E006840[] = {
+#include "levels/rr/rr_1_0xe006840_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_1__texture_0E00D040[] = {
+#include "levels/rr/rr_1_0xe00d040_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_1__texture_0E001020[] = {
+#include "levels/rr/rr_1_0xe001020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_1__texture_0E00A040[] = {
+#include "levels/rr/rr_1_0xe00a040_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_1__texture_0E00A840[] = {
+#include "levels/rr/rr_1_0xe00a840_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_1__texture_0E002820[] = {
+#include "levels/rr/rr_1_0xe002820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_1__texture_0E00B040[] = {
+#include "levels/rr/rr_1_0xe00b040_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_1__texture_0E004820[] = {
+#include "levels/rr/rr_1_0xe004820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_1__texture_0E005020[] = {
+#include "levels/rr/rr_1_0xe005020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_1__texture_0E009840[] = {
+#include "levels/rr/rr_1_0xe009840_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E005A20[] = {
+#include "levels/rr/rr_2_0xe005a20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E003220[] = {
+#include "levels/rr/rr_2_0xe003220_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E014080[] = {
+#include "levels/rr/rr_2_0xe014080_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E0150A0[] = {
+#include "levels/rr/rr_2_0xe0150a0_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E013880[] = {
+#include "levels/rr/rr_2_0xe013880_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E014880[] = {
+#include "levels/rr/rr_2_0xe014880_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E0158A0[] = {
+#include "levels/rr/rr_2_0xe0158a0_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E000220[] = {
+#include "levels/rr/rr_2_0xe000220_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E00D080[] = {
+#include "levels/rr/rr_2_0xe00d080_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E00E880[] = {
+#include "levels/rr/rr_2_0xe00e880_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E00B260[] = {
+#include "levels/rr/rr_2_0xe00b260_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E010880[] = {
+#include "levels/rr/rr_2_0xe010880_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E007240[] = {
+#include "levels/rr/rr_2_0xe007240_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E006A20[] = {
+#include "levels/rr/rr_2_0xe006a20_custom.ci4.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E007220 = {
+#include "levels/rr/rr_2_0xe006a20_custom.ci4.pal.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E008240[] = {
+#include "levels/rr/rr_2_0xe008240_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E00F880[] = {
+#include "levels/rr/rr_2_0xe00f880_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E00E080[] = {
+#include "levels/rr/rr_2_0xe00e080_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E009240[] = {
+#include "levels/rr/rr_2_0xe009240_custom.ci4.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E009A40 = {
+#include "levels/rr/rr_2_0xe009240_custom.ci4.pal.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E00D880[] = {
+#include "levels/rr/rr_2_0xe00d880_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E007A40[] = {
+#include "levels/rr/rr_2_0xe007a40_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E012880[] = {
+#include "levels/rr/rr_2_0xe012880_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E00F080[] = {
+#include "levels/rr/rr_2_0xe00f080_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E004A20[] = {
+#include "levels/rr/rr_2_0xe004a20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E005220[] = {
+#include "levels/rr/rr_2_0xe005220_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E009A60[] = {
+#include "levels/rr/rr_2_0xe009a60_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E012080[] = {
+#include "levels/rr/rr_2_0xe012080_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E00BC80[] = {
+#include "levels/rr/rr_2_0xe00bc80_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E00BA60[] = {
+#include "levels/rr/rr_2_0xe00ba60_custom.ci4.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E00BC60 = {
+#include "levels/rr/rr_2_0xe00ba60_custom.ci4.pal.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E002220[] = {
+#include "levels/rr/rr_2_0xe002220_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E001220[] = {
+#include "levels/rr/rr_2_0xe001220_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E004220[] = {
+#include "levels/rr/rr_2_0xe004220_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E002A20[] = {
+#include "levels/rr/rr_2_0xe002a20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E003A20[] = {
+#include "levels/rr/rr_2_0xe003a20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E011880[] = {
+#include "levels/rr/rr_2_0xe011880_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E00A260[] = {
+#include "levels/rr/rr_2_0xe00a260_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E000018[] = {
+#include "levels/rr/rr_2_0xe000018_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E00CC80[] = {
+#include "levels/rr/rr_2_0xe00cc80_custom.ia8.inc.c"
+};
+ALIGNED8 u8 rr_2__texture_0E011080[] = {
+#include "levels/rr/rr_2_0xe011080_custom.rgba16.inc.c"
+};
