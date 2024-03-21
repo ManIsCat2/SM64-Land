ad38e0b28a5cd083dee72b24115ba1ce64ac6ca9
ManIsCat2 <137772623+ManIsCat2@users.noreply.github.com>
2024-03-22 02:01:12 +0330

    add one more level :D

---
+++ levels/sa/texture.inc.c
@@ -0,0 +1,165 @@
+ALIGNED8 u8 sa_1__texture_0E001020[] = {
+#include "levels/sa/sa_1_0xe001020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E018C20[] = {
+#include "levels/sa/sa_1_0xe018c20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E015080[] = {
+#include "levels/sa/sa_1_0xe015080_custom.ci4.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E015280 = {
+#include "levels/sa/sa_1_0xe015080_custom.ci4.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E011480[] = {
+#include "levels/sa/sa_1_0xe011480_custom.ia8.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E019420[] = {
+#include "levels/sa/sa_1_0xe019420_custom.ia8.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E016B00[] = {
+#include "levels/sa/sa_1_0xe016b00_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E00DC80[] = {
+#include "levels/sa/sa_1_0xe00dc80_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E00F480[] = {
+#include "levels/sa/sa_1_0xe00f480_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E011C80[] = {
+#include "levels/sa/sa_1_0xe011c80_custom.ia8.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E012480[] = {
+#include "levels/sa/sa_1_0xe012480_custom.ia8.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E014880[] = {
+#include "levels/sa/sa_1_0xe014880_custom.ia8.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E013880[] = {
+#include "levels/sa/sa_1_0xe013880_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E000018[] = {
+#include "levels/sa/sa_1_0xe000018_custom.ia8.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E00E480[] = {
+#include "levels/sa/sa_1_0xe00e480_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E010C80[] = {
+#include "levels/sa/sa_1_0xe010c80_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E002040[] = {
+#include "levels/sa/sa_1_0xe002040_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E0062A0[] = {
+#include "levels/sa/sa_1_0xe0062a0_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E00BD00[] = {
+#include "levels/sa/sa_1_0xe00bd00_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E003860[] = {
+#include "levels/sa/sa_1_0xe003860_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E005080[] = {
+#include "levels/sa/sa_1_0xe005080_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E003040[] = {
+#include "levels/sa/sa_1_0xe003040_custom.ci4.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E003840 = {
+#include "levels/sa/sa_1_0xe003040_custom.ci4.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E017C20[] = {
+#include "levels/sa/sa_1_0xe017c20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E0084E0[] = {
+#include "levels/sa/sa_1_0xe0084e0_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E00A900[] = {
+#include "levels/sa/sa_1_0xe00a900_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E0072C0[] = {
+#include "levels/sa/sa_1_0xe0072c0_custom.ci4.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E0074C0 = {
+#include "levels/sa/sa_1_0xe0072c0_custom.ci4.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E00CC80[] = {
+#include "levels/sa/sa_1_0xe00cc80_custom.ia8.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E006080[] = {
+#include "levels/sa/sa_1_0xe006080_custom.ci4.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E006280 = {
+#include "levels/sa/sa_1_0xe006080_custom.ci4.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E004860[] = {
+#include "levels/sa/sa_1_0xe004860_custom.ci4.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E005060 = {
+#include "levels/sa/sa_1_0xe004860_custom.ci4.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E00FC80[] = {
+#include "levels/sa/sa_1_0xe00fc80_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E015AC0[] = {
+#include "levels/sa/sa_1_0xe015ac0_custom.ci4.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E0162C0 = {
+#include "levels/sa/sa_1_0xe015ac0_custom.ci4.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E0152A0[] = {
+#include "levels/sa/sa_1_0xe0152a0_custom.ci4.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E015AA0 = {
+#include "levels/sa/sa_1_0xe0152a0_custom.ci4.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E0162E0[] = {
+#include "levels/sa/sa_1_0xe0162e0_custom.ci4.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E016AE0 = {
+#include "levels/sa/sa_1_0xe0162e0_custom.ci4.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E0074E0[] = {
+#include "levels/sa/sa_1_0xe0074e0_custom.ia4.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E00A100[] = {
+#include "levels/sa/sa_1_0xe00a100_custom.ia8.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E001820[] = {
+#include "levels/sa/sa_1_0xe001820_custom.ci4.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E002020 = {
+#include "levels/sa/sa_1_0xe001820_custom.ci4.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E009CE0[] = {
+#include "levels/sa/sa_1_0xe009ce0_custom.ci4.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E00A0E0 = {
+#include "levels/sa/sa_1_0xe009ce0_custom.ci4.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E017B00[] = {
+#include "levels/sa/sa_1_0xe017b00_custom.ci4.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E017C00 = {
+#include "levels/sa/sa_1_0xe017b00_custom.ci4.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E00EC80[] = {
+#include "levels/sa/sa_1_0xe00ec80_custom.ia8.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E008CE0[] = {
+#include "levels/sa/sa_1_0xe008ce0_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E00B100[] = {
+#include "levels/sa/sa_1_0xe00b100_custom.ia8.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E006AA0[] = {
+#include "levels/sa/sa_1_0xe006aa0_custom.ci4.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E0072A0 = {
+#include "levels/sa/sa_1_0xe006aa0_custom.ci4.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E00B500[] = {
+#include "levels/sa/sa_1_0xe00b500_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sa_1__texture_0E012880[] = {
+#include "levels/sa/sa_1_0xe012880_custom.ia8.inc.c"
+};
