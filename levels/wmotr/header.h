b3e30a9567c1a0cb94130286fe926b7c52f1ab73
ManIsCat2 <137772623+ManIsCat2@users.noreply.github.com>
2024-03-22 15:27:11 +0330

    new level

---
+++ levels/wmotr/header.h
@@ -0,0 +1,37 @@
+#ifndef WMOTR_HEADER_H
+#define WMOTR_HEADER_H
+#include "types.h"
+#include "game/moving_texture.h"
+extern const GeoLayout Geo_wmotr_1_0x2043ff0[];
+extern Gfx DL_wmotr_1_0xe075130[];
+extern Gfx DL_wmotr_1_0xe085dd0[];
+extern Gfx DL_wmotr_1_0xe086578[];
+extern const Collision col_wmotr_1_0xe0867e0[];
+extern struct MovtexQuadCollection wmotr_1_Movtex_0[];
+extern struct MovtexQuadCollection wmotr_1_Movtex_1[];
+extern struct MovtexQuadCollection wmotr_1_Movtex_2[];
+extern const GeoLayout Geo_wmotr_2_0x20441d0[];
+extern Gfx DL_wmotr_2_0xe0747f0[];
+extern Gfx DL_wmotr_2_0xe08ab10[];
+extern Gfx DL_wmotr_2_0xe08ad68[];
+extern Gfx DL_wmotr_2_0x7025340[];
+extern const Collision col_wmotr_2_0xe08b000[];
+extern struct MovtexQuadCollection wmotr_2_Movtex_0[];
+extern struct MovtexQuadCollection wmotr_2_Movtex_1[];
+extern struct MovtexQuadCollection wmotr_2_Movtex_2[];
+extern const LevelScript level_wmotr_custom_entry[];
+extern const LevelScript local_area_wmotr_1_[];
+extern const LevelScript local_objects_wmotr_1_[];
+extern const LevelScript local_warps_wmotr_1_[];
+extern const LevelScript local_area_wmotr_2_[];
+extern const LevelScript local_objects_wmotr_2_[];
+extern const LevelScript local_warps_wmotr_2_[];
+extern const LevelScript level_wmotr_entry[];
+extern const GeoLayout wmotr_geo_0001F0[];
+extern const Gfx wmotr_seg7_dl_0700C1F8[];
+extern const Gfx wmotr_seg7_dl_0700EFD8[];
+extern const Gfx wmotr_seg7_dl_07010608[];
+extern const Collision wmotr_seg7_collision[];
+extern const MacroObject wmotr_seg7_macro_objs[];
+extern const LevelScript level_wmotr_entry[];
+#endif
