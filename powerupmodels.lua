94f376243d67aa4585496c69113a73007144e15f
xLuigiGamerx <mohammmedsadawi666@gmail.com>
2024-03-22 20:23:58 +0300

    Rewrote and optimized the powerups system

--- powerupmodels.lua
+++ powerupmodels.lua
@@ -1,6 +1,32 @@
 -- Placed here to remove the need of setting models for every powerup
 -- Uses the definitive model check
 
+-- Models
+
+E_MODEL_CAT_MARIO = smlua_model_util_get_id("cat_mario_geo")
+E_MODEL_TANOOKI_MARIO = smlua_model_util_get_id("tanooki_mario_geo")
+
+-- Powerups enum
+
+NORMAL = 0
+TANOOKI = 1
+CAT = 2
+
+activePowerup = NORMAL -- Current Powerup, set to NORMAL to disable any powerup
+
+-- Powerup States, to add more powerups here, you must first add them to the enum and assign a number
+powerupStates = {
+    [NORMAL] = {modelId = nil},
+    [TANOOKI] = {modelId = E_MODEL_TANOOKI_MARIO},
+    [CAT] = {modelId = E_MODEL_CAT_MARIO}
+}
+
+-- Powerup Model Functions --
+
+function powerups()
+    gPlayerSyncTable[0].modelId = powerupStates[activePowerup].modelId
+end
+
 hook_event(HOOK_OBJECT_SET_MODEL, function (o)
     if obj_has_behavior_id(o, id_bhvMario) ~= 0 then
         local i = network_local_index_from_global(o.globalPlayerIndex)
@@ -13,34 +39,25 @@ hook_event(HOOK_OBJECT_SET_MODEL, function (o)
     end
 end)
 
-powerup = false
-
-function powerup_check()
-    if powerup == false then
-        cloud = false
-        bee = false
-        fludd = false
-        tanooki = false
-        catsuit = false
-        gPlayerSyncTable[0].modelId = nil
-    end
-end
+-- Resets the player stats when leaving a level or dying
 
 function on_death_warp()
-    powerup = false
+    activePowerup = NORMAL
     timer = 0
     score = 0
     scoreCounter = 0
 end
 
+-- Removes the player's powerup on damage
+
 function damage_check(m)
     if m.playerIndex ~= 0 then return end
     if m.hurtCounter > 0 or m.action == ACT_BURNING_GROUND or m.action == ACT_BURNING_JUMP then
-        powerup = false
+        activePowerup = NORMAL
     end
 end
 
-hook_event(HOOK_UPDATE, powerup_check)
+hook_event(HOOK_UPDATE, powerups)
 hook_event(HOOK_MARIO_UPDATE, damage_check)
 hook_event(HOOK_ON_DEATH, on_death_warp)
 hook_event(HOOK_ON_WARP, on_death_warp)
