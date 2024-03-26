16bd39c1826a56348a60d4aa206953891aaee098
ManIsCat2 <137772623+ManIsCat2@users.noreply.github.com>
2024-03-26 17:10:53 +0330

    automatic star recoloring

--- recolored_stars.lua
+++ recolored_stars.lua
@@ -11,7 +11,7 @@ E_MODEL_STAR_W7 = smlua_model_util_get_id("star_w7_geo")
 E_MODEL_STAR_W8 = smlua_model_util_get_id("star_w8_geo")
 
 local worldSpecificStarModels = {
-    nil,
+    E_MODEL_STAR,
     E_MODEL_STAR_W2,
     E_MODEL_STAR_W3,
     E_MODEL_STAR_W4,
@@ -21,15 +21,26 @@ local worldSpecificStarModels = {
     E_MODEL_STAR_W8
 }
 
+local courseToStarModel = {
+    [COURSE_BOB] = E_MODEL_STAR,
+    [COURSE_WF] = E_MODEL_STAR,
+    [COURSE_CCM] = E_MODEL_STAR_W2,
+    [COURSE_JRB] = E_MODEL_STAR_W2,
+    [COURSE_BBH] = E_MODEL_STAR_W3,
+    [COURSE_LLL] = E_MODEL_STAR_W3,
+    [COURSE_SSL] = E_MODEL_STAR_W3,
+    [COURSE_HMC] = E_MODEL_STAR_W3,
+}
+
 -- Model Replacement Function --
 
 function bhv_star_loop(o)
     if obj_has_model_extended(o, E_MODEL_TRANSPARENT_STAR) ~= 0 then return end
-    if gNetworkPlayers[0].currLevelNum == LEVEL_CCM or gNetworkPlayers[0].currLevelNum == LEVEL_JRB then
-        obj_set_model_extended(o, E_MODEL_STAR_W2)
-    end
+
     if (gNetworkPlayers[0].currLevelNum == LEVEL_CASTLE_GROUNDS or gNetworkPlayers[0].currLevelNum == LEVEL_CASTLE_COURTYARD) and worldSpecificStarModels[curWorld] ~= nil then
         obj_set_model_extended(o, worldSpecificStarModels[curWorld])
+    else
+        obj_set_model_extended(o, courseToStarModel[gNetworkPlayers[0].currCourseNum])
     end
 end
 
