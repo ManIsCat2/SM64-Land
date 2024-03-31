6d7f7d2c3e785df0231b979a4f973ca1dc31e516
ManIsCat2 <137772623+ManIsCat2@users.noreply.github.com>
2024-04-01 00:02:13 +0330

    Add green switchboards From MOPs

---
+++ levels/sl/texture.inc.c
@@ -0,0 +1,120 @@
+ALIGNED8 u8 sl_1__texture_0E006C20[] = {
+#include "levels/sl/sl_1_0xe006c20_custom.ci4.inc.c"
+};
+ALIGNED8 u8 sl_1__texture_0E007420 = {
+#include "levels/sl/sl_1_0xe006c20_custom.ci4.inc.c"
+};
+ALIGNED8 u8 sl_1__texture_0E007C40[] = {
+#include "levels/sl/sl_1_0xe007c40_custom.ci4.inc.c"
+};
+ALIGNED8 u8 sl_1__texture_0E008440 = {
+#include "levels/sl/sl_1_0xe007c40_custom.ci4.inc.c"
+};
+ALIGNED8 u8 sl_1__texture_0E008460[] = {
+#include "levels/sl/sl_1_0xe008460_custom.ia8.inc.c"
+};
+ALIGNED8 u8 sl_1__texture_0E000018[] = {
+#include "levels/sl/sl_1_0xe000018_custom.ia4.inc.c"
+};
+ALIGNED8 u8 sl_1__texture_0E005C20[] = {
+#include "levels/sl/sl_1_0xe005c20_custom.ia8.inc.c"
+};
+ALIGNED8 u8 sl_1__texture_0E000820[] = {
+#include "levels/sl/sl_1_0xe000820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sl_1__texture_0E004C20[] = {
+#include "levels/sl/sl_1_0xe004c20_custom.ia8.inc.c"
+};
+ALIGNED8 u8 sl_1__texture_0E001C20[] = {
+#include "levels/sl/sl_1_0xe001c20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sl_1__texture_0E001420[] = {
+#include "levels/sl/sl_1_0xe001420_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sl_1__texture_0E002420[] = {
+#include "levels/sl/sl_1_0xe002420_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sl_1__texture_0E001020[] = {
+#include "levels/sl/sl_1_0xe001020_custom.ia8.inc.c"
+};
+ALIGNED8 u8 sl_1__texture_0E002C20[] = {
+#include "levels/sl/sl_1_0xe002c20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sl_1__texture_0E003420[] = {
+#include "levels/sl/sl_1_0xe003420_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sl_1__texture_0E008C60[] = {
+#include "levels/sl/sl_1_0xe008c60_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sl_1__texture_0E009460[] = {
+#include "levels/sl/sl_1_0xe009460_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sl_1__texture_0E007440[] = {
+#include "levels/sl/sl_1_0xe007440_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sl_1__texture_0E004420[] = {
+#include "levels/sl/sl_1_0xe004420_custom.ia4.inc.c"
+};
+ALIGNED8 u8 sl_2__texture_0E000420[] = {
+#include "levels/sl/sl_2_0xe000420_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sl_2__texture_0E001E20[] = {
+#include "levels/sl/sl_2_0xe001e20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sl_2__texture_0E000E20[] = {
+#include "levels/sl/sl_2_0xe000e20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sl_2__texture_0E000C20[] = {
+#include "levels/sl/sl_2_0xe000c20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sl_2__texture_0E002620[] = {
+#include "levels/sl/sl_2_0xe002620_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sl_2__texture_0E006840[] = {
+#include "levels/sl/sl_2_0xe006840_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sl_2__texture_0E005620[] = {
+#include "levels/sl/sl_2_0xe005620_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sl_2__texture_0E00AA40[] = {
+#include "levels/sl/sl_2_0xe00aa40_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sl_2__texture_0E007440[] = {
+#include "levels/sl/sl_2_0xe007440_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sl_2__texture_0E007E40[] = {
+#include "levels/sl/sl_2_0xe007e40_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sl_2__texture_0E009640[] = {
+#include "levels/sl/sl_2_0xe009640_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sl_2__texture_0E009840[] = {
+#include "levels/sl/sl_2_0xe009840_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sl_2__texture_0E004E20[] = {
+#include "levels/sl/sl_2_0xe004e20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sl_2__texture_0E003620[] = {
+#include "levels/sl/sl_2_0xe003620_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sl_2__texture_0E006640[] = {
+#include "levels/sl/sl_2_0xe006640_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sl_2__texture_0E007C40[] = {
+#include "levels/sl/sl_2_0xe007c40_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sl_2__texture_0E00A840[] = {
+#include "levels/sl/sl_2_0xe00a840_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sl_2__texture_0E007040[] = {
+#include "levels/sl/sl_2_0xe007040_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sl_2__texture_0E004620[] = {
+#include "levels/sl/sl_2_0xe004620_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sl_2__texture_0E008640[] = {
+#include "levels/sl/sl_2_0xe008640_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 sl_2__texture_0E000018[] = {
+#include "levels/sl/sl_2_0xe000018_custom.ia8.inc.c"
+};
