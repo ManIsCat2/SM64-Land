6a2e53f96fe70c0d646b3f13d5fecdf488a9d3f6
ManIsCat2 <137772623+ManIsCat2@users.noreply.github.com>
2024-04-04 13:58:32 +0330

    RM2C OUTPUT FOR BOB LMAO NO WAY

---
+++ levels/bob/textureNew.inc.c
@@ -0,0 +1,192 @@
+ALIGNED8 u8 bob_1__texture_0E008220[] = {
+#include "levels/bob/bob_1_0xe008220_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_1__texture_0E004810[] = {
+#include "levels/bob/bob_1_0xe004810_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_1__texture_0E00B220[] = {
+#include "levels/bob/bob_1_0xe00b220_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_1__texture_0E001010[] = {
+#include "levels/bob/bob_1_0xe001010_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_1__texture_0E006010[] = {
+#include "levels/bob/bob_1_0xe006010_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_1__texture_0E00BA20[] = {
+#include "levels/bob/bob_1_0xe00ba20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_1__texture_0E008A20[] = {
+#include "levels/bob/bob_1_0xe008a20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_1__texture_0E002810[] = {
+#include "levels/bob/bob_1_0xe002810_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_1__texture_0E00A220[] = {
+#include "levels/bob/bob_1_0xe00a220_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_1__texture_0E003810[] = {
+#include "levels/bob/bob_1_0xe003810_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_1__texture_0E007A20[] = {
+#include "levels/bob/bob_1_0xe007a20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_1__texture_0E009A20[] = {
+#include "levels/bob/bob_1_0xe009a20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_1__texture_0E002010[] = {
+#include "levels/bob/bob_1_0xe002010_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_1__texture_0E012220[] = {
+#include "levels/bob/bob_1_0xe012220_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_1__texture_0E005010[] = {
+#include "levels/bob/bob_1_0xe005010_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_1__texture_0E00D220[] = {
+#include "levels/bob/bob_1_0xe00d220_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_1__texture_0E010220[] = {
+#include "levels/bob/bob_1_0xe010220_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_1__texture_0E00F220[] = {
+#include "levels/bob/bob_1_0xe00f220_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_1__texture_0E011220[] = {
+#include "levels/bob/bob_1_0xe011220_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_1__texture_0E011A20[] = {
+#include "levels/bob/bob_1_0xe011a20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_1__texture_0E00C220[] = {
+#include "levels/bob/bob_1_0xe00c220_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_1__texture_0E003010[] = {
+#include "levels/bob/bob_1_0xe003010_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_1__texture_0E000810[] = {
+#include "levels/bob/bob_1_0xe000810_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_1__texture_0E000010[] = {
+#include "levels/bob/bob_1_0xe000010_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_1__texture_0E00E220[] = {
+#include "levels/bob/bob_1_0xe00e220_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_1__texture_0E007010[] = {
+#include "levels/bob/bob_1_0xe007010_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_1__texture_0E007810[] = {
+#include "levels/bob/bob_1_0xe007810_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_2__texture_0E000018[] = {
+#include "levels/bob/bob_2_0xe000018_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_2__texture_0E002820[] = {
+#include "levels/bob/bob_2_0xe002820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_2__texture_0E000820[] = {
+#include "levels/bob/bob_2_0xe000820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_2__texture_0E006820[] = {
+#include "levels/bob/bob_2_0xe006820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_2__texture_0E007820[] = {
+#include "levels/bob/bob_2_0xe007820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_2__texture_0E004020[] = {
+#include "levels/bob/bob_2_0xe004020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_2__texture_0E00C020[] = {
+#include "levels/bob/bob_2_0xe00c020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_2__texture_0E00B020[] = {
+#include "levels/bob/bob_2_0xe00b020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_2__texture_0E00A820[] = {
+#include "levels/bob/bob_2_0xe00a820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_2__texture_0E009820[] = {
+#include "levels/bob/bob_2_0xe009820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_2__texture_0E005020[] = {
+#include "levels/bob/bob_2_0xe005020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_2__texture_0E005820[] = {
+#include "levels/bob/bob_2_0xe005820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_2__texture_0E001820[] = {
+#include "levels/bob/bob_2_0xe001820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_2__texture_0E008820[] = {
+#include "levels/bob/bob_2_0xe008820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_2__texture_0E003820[] = {
+#include "levels/bob/bob_2_0xe003820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_3__texture_0E000018[] = {
+#include "levels/bob/bob_3_0xe000018_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_3__texture_0E002040[] = {
+#include "levels/bob/bob_3_0xe002040_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_3__texture_0E003040[] = {
+#include "levels/bob/bob_3_0xe003040_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_3__texture_0E004840[] = {
+#include "levels/bob/bob_3_0xe004840_custom.ci4.inc.c"
+};
+ALIGNED8 u8 bob_3__texture_0E005040[] = {
+#include "levels/bob/bob_3_0xe004840_custom.ci4.inc.c"
+};
+ALIGNED8 u8 bob_3__texture_0E001020[] = {
+#include "levels/bob/bob_3_0xe001020_custom.ci4.inc.c"
+};
+ALIGNED8 u8 bob_3__texture_0E001820[] = {
+#include "levels/bob/bob_3_0xe001020_custom.ci4.inc.c"
+};
+ALIGNED8 u8 bob_3__texture_0E004040[] = {
+#include "levels/bob/bob_3_0xe004040_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_3__texture_0E001840[] = {
+#include "levels/bob/bob_3_0xe001840_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_4__texture_0E000018[] = {
+#include "levels/bob/bob_4_0xe000018_custom.i8.inc.c"
+};
+ALIGNED8 u8 bob_4__texture_0E000C20[] = {
+#include "levels/bob/bob_4_0xe000c20_custom.i8.inc.c"
+};
+ALIGNED8 u8 bob_4__texture_0E004D20[] = {
+#include "levels/bob/bob_4_0xe004d20_custom.i8.inc.c"
+};
+ALIGNED8 u8 bob_4__texture_0E003920[] = {
+#include "levels/bob/bob_4_0xe003920_custom.i8.inc.c"
+};
+ALIGNED8 u8 bob_4__texture_0E004520[] = {
+#include "levels/bob/bob_4_0xe004520_custom.i8.inc.c"
+};
+ALIGNED8 u8 bob_4__texture_0E003D20[] = {
+#include "levels/bob/bob_4_0xe003d20_custom.i8.inc.c"
+};
+ALIGNED8 u8 bob_4__texture_0E001C20[] = {
+#include "levels/bob/bob_4_0xe001c20_custom.i8.inc.c"
+};
+ALIGNED8 u8 bob_4__texture_0E005120[] = {
+#include "levels/bob/bob_4_0xe005120_custom.i8.inc.c"
+};
+ALIGNED8 u8 bob_4__texture_0E002D20[] = {
+#include "levels/bob/bob_4_0xe002d20_custom.i8.inc.c"
+};
+ALIGNED8 u8 bob_4__texture_0E002520[] = {
+#include "levels/bob/bob_4_0xe002520_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bob_4__texture_0E003120[] = {
+#include "levels/bob/bob_4_0xe003120_custom.i8.inc.c"
+};
+ALIGNED8 u8 bob_4__texture_0E000820[] = {
+#include "levels/bob/bob_4_0xe000820_custom.ia8.inc.c"
+};
+ALIGNED8 u8 bob_4__texture_0E002420[] = {
+#include "levels/bob/bob_4_0xe002420_custom.ia8.inc.c"
+};
