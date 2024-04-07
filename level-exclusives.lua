ca90f3e1c523031cd9b633c6bfb77f8a5c650da7
ManIsCat2 <137772623+ManIsCat2@users.noreply.github.com>
2024-04-07 18:52:29 +0330

    Fix low-grav affecting twirling

--- level-exclusives.lua
+++ level-exclusives.lua
@@ -1,5 +1,5 @@
 function level_1_3(m)
-    if m.action & ACT_FLAG_AIR ~= 0 and gNetworkPlayers[0].currLevelNum == LEVEL_WF and gNetworkPlayers[0].currAreaIndex == 2 then
+    if m.action & ACT_FLAG_AIR ~= 0 and gNetworkPlayers[0].currLevelNum == LEVEL_WF and gNetworkPlayers[0].currAreaIndex == 2 and m.action ~= ACT_TWIRLING then
         m.vel.y = m.vel.y + 1.2
     end
 end
