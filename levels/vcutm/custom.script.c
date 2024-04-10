81655779fb3ce0c292fa5f6a700c911f4f09ed46
ManIsCat2 <137772623+ManIsCat2@users.noreply.github.com>
2024-04-10 13:34:45 +0330

    add VCUTM

---
+++ levels/vcutm/custom.script.c
@@ -0,0 +1,119 @@
+#include <ultra64.h>
+#include "sm64.h"
+#include "behavior_data.h"
+#include "model_ids.h"
+#include "seq_ids.h"
+#include "dialog_ids.h"
+#include "segment_symbols.h"
+#include "level_commands.h"
+#include "game/level_update.h"
+#include "levels/scripts.h"
+#include "actors/common1.h"
+#include "make_const_nonconst.h"
+
+#include "areas/1/custom.model.inc.h"
+#include "areas/2/custom.model.inc.h"
+#include "areas/3/custom.model.inc.h"
+#include "levels/vcutm/header.h"
+extern u8 _vcutm_segment_ESegmentRomStart[]; 
+extern u8 _vcutm_segment_ESegmentRomEnd[];
+const LevelScript level_vcutm_entry[] = {
+INIT_LEVEL(),
+LOAD_RAW(0x0E, _vcutm_segment_ESegmentRomStart, _vcutm_segment_ESegmentRomEnd),
+LOAD_MIO0(0xA,_water_skybox_mio0SegmentRomStart,_water_skybox_mio0SegmentRomEnd),
+LOAD_MIO0(8,_common0_mio0SegmentRomStart,_common0_mio0SegmentRomEnd),
+LOAD_RAW(15,_common0_geoSegmentRomStart,_common0_geoSegmentRomEnd),
+LOAD_MIO0(6,_group15_mio0SegmentRomStart,_group15_mio0SegmentRomEnd),
+LOAD_RAW(13,_group15_geoSegmentRomStart,_group15_geoSegmentRomEnd),
+ALLOC_LEVEL_POOL(),
+MARIO(/*model*/ MODEL_MARIO, /*behParam*/ 0x00000001, /*beh*/ bhvMario),
+LOAD_MODEL_FROM_GEO(22, warp_pipe_geo),
+LOAD_MODEL_FROM_GEO(23, bubbly_tree_geo),
+LOAD_MODEL_FROM_GEO(24, spiky_tree_geo),
+LOAD_MODEL_FROM_GEO(25, snow_tree_geo),
+LOAD_MODEL_FROM_GEO(27, palm_tree_geo),
+LOAD_MODEL_FROM_GEO(31, metal_door_geo),
+LOAD_MODEL_FROM_GEO(32, hazy_maze_door_geo),
+LOAD_MODEL_FROM_GEO(34, castle_door_0_star_geo),
+LOAD_MODEL_FROM_GEO(35, castle_door_1_star_geo),
+LOAD_MODEL_FROM_GEO(36, castle_door_3_stars_geo),
+LOAD_MODEL_FROM_GEO(37, key_door_geo),
+LOAD_MODEL_FROM_GEO(38, castle_door_geo),
+// LOAD_MODEL_FROM_DL(132,0x08025f08,4),
+// LOAD_MODEL_FROM_DL(158,0x0302c8a0,4),
+// LOAD_MODEL_FROM_DL(159,0x0302bcd0,4),
+// LOAD_MODEL_FROM_DL(161,0x0301cb00,4),
+// LOAD_MODEL_FROM_DL(164,0x04032a18,4),
+// LOAD_MODEL_FROM_DL(201,0x080048e0,4),
+// LOAD_MODEL_FROM_DL(218,0x08024bb8,4),
+// LOAD_MODEL_FROM_GEO(239,0x04008090),
+// LOAD_MODEL_FROM_GEO(240,0x0040ef00),
+// LOAD_MODEL_FROM_GEO(241,0x00405e00),
+// LOAD_MODEL_FROM_GEO(242,0x0040eb80),
+// LOAD_MODEL_FROM_GEO(243,0x0040fd50),
+// LOAD_MODEL_FROM_GEO(244,0x0040ebd0),
+// LOAD_MODEL_FROM_GEO(245,0x003767d4),
+// LOAD_MODEL_FROM_GEO(246,0x0040ed50),
+// LOAD_MODEL_FROM_GEO(247,0x00412a30),
+// LOAD_MODEL_FROM_GEO(248,0x00413f50),
+// LOAD_MODEL_FROM_GEO(249,0x0040eac0),
+// LOAD_MODEL_FROM_GEO(250,0x0040e880),
+JUMP_LINK(script_func_global_1),
+JUMP_LINK(script_func_global_16),
+JUMP_LINK(local_area_vcutm_1_),
+JUMP_LINK(local_area_vcutm_2_),
+FREE_LEVEL_POOL(),
+MARIO_POS(1,0,0,0,0),
+CALL(/*arg*/ 0, /*func*/ lvl_init_or_update),
+CALL_LOOP(/*arg*/ 1, /*func*/ lvl_init_or_update),
+CLEAR_LEVEL(),
+SLEEP_BEFORE_EXIT(/*frames*/ 1),
+EXIT(),
+};
+const LevelScript local_area_vcutm_1_[] = {
+AREA(1,Geo_vcutm_1_0x25b4720),
+TERRAIN(col_vcutm_1_0xe011150),
+SET_BACKGROUND_MUSIC(0,59),
+TERRAIN_TYPE(0),
+JUMP_LINK(local_objects_vcutm_1_),
+JUMP_LINK(local_warps_vcutm_1_),
+END_AREA(),
+RETURN()
+};
+const LevelScript local_objects_vcutm_1_[] = {
+OBJECT_WITH_ACTS(0,-1638,0,98,0,90,0,0xa0000, bhvSpinAirborneWarp,31),
+OBJECT_WITH_ACTS(E_MODEL_TOAD,-246,130,49,0,270,0,0x18000001, id_bhvToadMessage,31),
+OBJECT_WITH_ACTS(0,-2142,0,62,0,90,0,0xa0a0000, bhvWarp,31),
+OBJECT_WITH_ACTS(0,0,0,0,0,0,0,0x0, Bhv_Custom_0x0041f5a0,31),
+RETURN()
+};
+const LevelScript local_warps_vcutm_1_[] = {
+WARP_NODE(10,16,4,5,0),
+WARP_NODE(240,6,1,50,0),
+WARP_NODE(241,6,1,100,0),
+RETURN()
+};
+const LevelScript local_area_vcutm_2_[] = {
+AREA(2,Geo_vcutm_2_0x25b4900),
+TERRAIN(col_vcutm_2_0xe032ec0),
+SET_BACKGROUND_MUSIC(0,59),
+TERRAIN_TYPE(0),
+JUMP_LINK(local_objects_vcutm_2_),
+JUMP_LINK(local_warps_vcutm_2_),
+END_AREA(),
+RETURN()
+};
+const LevelScript local_objects_vcutm_2_[] = {
+OBJECT_WITH_ACTS(0,-1638,0,98,0,90,0,0xa0000, bhvSpinAirborneWarp,31),
+OBJECT_WITH_ACTS(E_MODEL_TOAD,-246,130,49,0,270,0,0x1c000203, id_bhvToadMessage,31),
+OBJECT_WITH_ACTS(0,-2142,0,62,0,90,0,0xa0a0000, bhvWarp,31),
+OBJECT_WITH_ACTS(0,0,0,0,0,0,0,0x0, Bhv_Custom_0x070266e0,31),
+OBJECT_WITH_ACTS(0,0,0,0,0,0,0,0x0, Bhv_Custom_0x0041f5a0,31),
+RETURN()
+};
+const LevelScript local_warps_vcutm_2_[] = {
+WARP_NODE(10,26,4,89,0),
+WARP_NODE(240,6,1,50,0),
+WARP_NODE(241,6,1,100,0),
+RETURN()
+};
---
+++ levels/vcutm/textureNew.inc.c
@@ -0,0 +1,129 @@
+ALIGNED8 u8 vcutm_1__texture_0E001020[] = {
+#include "levels/vcutm/vcutm_1_0xe001020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 vcutm_1__texture_0E000820[] = {
+#include "levels/vcutm/vcutm_1_0xe000820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 vcutm_1__texture_0E004020[] = {
+#include "levels/vcutm/vcutm_1_0xe004020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 vcutm_1__texture_0E007C20[] = {
+#include "levels/vcutm/vcutm_1_0xe007c20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 vcutm_1__texture_0E003820[] = {
+#include "levels/vcutm/vcutm_1_0xe003820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 vcutm_1__texture_0E008C20[] = {
+#include "levels/vcutm/vcutm_1_0xe008c20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 vcutm_1__texture_0E006C20[] = {
+#include "levels/vcutm/vcutm_1_0xe006c20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 vcutm_1__texture_0E005C20[] = {
+#include "levels/vcutm/vcutm_1_0xe005c20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 vcutm_1__texture_0E005420[] = {
+#include "levels/vcutm/vcutm_1_0xe005420_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 vcutm_1__texture_0E009420[] = {
+#include "levels/vcutm/vcutm_1_0xe009420_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 vcutm_1__texture_0E006420[] = {
+#include "levels/vcutm/vcutm_1_0xe006420_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 vcutm_1__texture_0E004C20[] = {
+#include "levels/vcutm/vcutm_1_0xe004c20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 vcutm_1__texture_0E000018[] = {
+#include "levels/vcutm/vcutm_1_0xe000018_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 vcutm_1__texture_0E004820[] = {
+#include "levels/vcutm/vcutm_1_0xe004820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 vcutm_1__texture_0E003020[] = {
+#include "levels/vcutm/vcutm_1_0xe003020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 vcutm_1__texture_0E002020[] = {
+#include "levels/vcutm/vcutm_1_0xe002020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 vcutm_2__texture_0E004820[] = {
+#include "levels/vcutm/vcutm_2_0xe004820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 vcutm_2__texture_0E005020[] = {
+#include "levels/vcutm/vcutm_2_0xe005020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 vcutm_2__texture_0E004020[] = {
+#include "levels/vcutm/vcutm_2_0xe004020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 vcutm_2__texture_0E007820[] = {
+#include "levels/vcutm/vcutm_2_0xe007820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 vcutm_2__texture_0E007020[] = {
+#include "levels/vcutm/vcutm_2_0xe007020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 vcutm_2__texture_0E000018[] = {
+#include "levels/vcutm/vcutm_2_0xe000018_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 vcutm_2__texture_0E000820[] = {
+#include "levels/vcutm/vcutm_2_0xe000820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 vcutm_2__texture_0E008820[] = {
+#include "levels/vcutm/vcutm_2_0xe008820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 vcutm_2__texture_0E006820[] = {
+#include "levels/vcutm/vcutm_2_0xe006820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 vcutm_2__texture_0E006020[] = {
+#include "levels/vcutm/vcutm_2_0xe006020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 vcutm_2__texture_0E002820[] = {
+#include "levels/vcutm/vcutm_2_0xe002820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 vcutm_2__texture_0E003020[] = {
+#include "levels/vcutm/vcutm_2_0xe003020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 vcutm_2__texture_0E003820[] = {
+#include "levels/vcutm/vcutm_2_0xe003820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 vcutm_2__texture_0E001820[] = {
+#include "levels/vcutm/vcutm_2_0xe001820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 vcutm_3__texture_0E003C30[] = {
+#include "levels/vcutm/vcutm_3_0xe003c30_custom.ia8.inc.c"
+};
+ALIGNED8 u8 vcutm_3__texture_0E003030[] = {
+#include "levels/vcutm/vcutm_3_0xe003030_custom.ia8.inc.c"
+};
+ALIGNED8 u8 vcutm_3__texture_0E002030[] = {
+#include "levels/vcutm/vcutm_3_0xe002030_custom.ia8.inc.c"
+};
+ALIGNED8 u8 vcutm_3__texture_0E005830[] = {
+#include "levels/vcutm/vcutm_3_0xe005830_custom.ia8.inc.c"
+};
+ALIGNED8 u8 vcutm_3__texture_0E004430[] = {
+#include "levels/vcutm/vcutm_3_0xe004430_custom.ia8.inc.c"
+};
+ALIGNED8 u8 vcutm_3__texture_0E003430[] = {
+#include "levels/vcutm/vcutm_3_0xe003430_custom.ia8.inc.c"
+};
+ALIGNED8 u8 vcutm_3__texture_0E004C30[] = {
+#include "levels/vcutm/vcutm_3_0xe004c30_custom.ia8.inc.c"
+};
+ALIGNED8 u8 vcutm_3__texture_0E001830[] = {
+#include "levels/vcutm/vcutm_3_0xe001830_custom.ia8.inc.c"
+};
+ALIGNED8 u8 vcutm_3__texture_0E000830[] = {
+#include "levels/vcutm/vcutm_3_0xe000830_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 vcutm_3__texture_0E004830[] = {
+#include "levels/vcutm/vcutm_3_0xe004830_custom.ia8.inc.c"
+};
+ALIGNED8 u8 vcutm_3__texture_0E000030[] = {
+#include "levels/vcutm/vcutm_3_0xe000030_custom.i8.inc.c"
+};
+ALIGNED8 u8 vcutm_3__texture_0E005430[] = {
+#include "levels/vcutm/vcutm_3_0xe005430_custom.ia8.inc.c"
+};
+ALIGNED8 u8 vcutm_3__texture_0E002830[] = {
+#include "levels/vcutm/vcutm_3_0xe002830_custom.ia8.inc.c"
+};
