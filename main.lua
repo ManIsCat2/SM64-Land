97d1fc54564634ec4df2b6580866dd59c63bd338
xLuigiGamerx <mohammmedsadawi666@gmail.com>
2024-05-05 23:25:53 +0300

    Disabled rounded corners and groundpound bonks

--- main.lua
+++ main.lua
@@ -21,6 +21,8 @@ end
 gLevelValues.exitCastleLevel = LEVEL_CASTLE_GROUNDS
 -- fix collisions being bad
 gLevelValues.fixCollisionBugs = true
+gLevelValues.fixCollisionBugsRoundedCorners = false
+gLevelValues.fixCollisionBugsGroundPoundBonks = true
 --fix  cap durations
 gLevelValues.metalCapDurationCotmc = 1
 gLevelValues.wingCapDurationTotwc = 1
@@ -37,13 +39,30 @@ save_file_set_flags(SAVE_FLAG_HAVE_WING_CAP)
 
 hook_event(HOOK_MARIO_UPDATE, mario_update)
 
--- World Map Camera --
+--[[ World Map Camera --
 function world_map_cam(m)
     if m.playerIndex ~= 0 then return end
+    l = gLakituState
     if gNetworkPlayers[0].currLevelNum ~= (LEVEL_CASTLE_GROUNDS) and gNetworkPlayers[0].currLevelNum ~= (LEVEL_CASTLE_COURTYARD) then return camera_unfreeze() end
     if m.action == ACT_READING_NPC_DIALOG or m.action == ACT_READING_SIGN then return camera_unfreeze() end
-    gLakituState.curPos.y = m.pos.y + 4000
-    gLakituState.curPos.z = 5000
+    vec3f_copy(l.focus, m.pos)
+    l.pos.x = m.pos.x
+    l.pos.y = m.pos.y + 1500
+    l.pos.z = m.pos.z + 4350
+    djui_chat_message_create("yaw: "..tostring(l.yaw))
+    djui_chat_message_create("nextYaw: "..tostring(l.nextYaw))
+    djui_chat_message_create("oldYaw: "..tostring(l.oldYaw))
+    djui_chat_message_create("yaw: "..tostring(l.yaw))
+    djui_chat_message_create("nextYaw: "..tostring(l.nextYaw))
+    djui_chat_message_create("oldYaw: "..tostring(l.oldYaw))
+    l.yaw = 0
+    l.oldYaw = -32768
+    l.nextYaw = 0
+    l.oldPitch = 0
+    l.roll = 0
+    l.oldRoll = 0
+    camera_freeze()
 end
 
---hook_event(HOOK_MARIO_UPDATE, world_map_cam)
+hook_event(HOOK_MARIO_UPDATE, world_map_cam)
+]]
