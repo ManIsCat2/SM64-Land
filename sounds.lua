a77dd052f77400dcbab31ebc51c60364aab6bdb0
MaiskX3 <maiskxdxd@gmail.com>
2024-09-14 21:30:32 -0300

    Labeled everything and changed soundbanks

--- sounds.lua
+++ sounds.lua
@@ -1,55 +1,55 @@
-smlua_audio_utils_replace_sequence(SEQ_LEVEL_GRASS, 37, 75, "SM3DWZeroVv2")
-smlua_audio_utils_replace_sequence(72, 26, 75, "1-2_Puzzle_Plank")
-smlua_audio_utils_replace_sequence(37, 26, 75, "Space1")
-smlua_audio_utils_replace_sequence(71, 26, 75, "SSBTargetTest_Andy_NI26")
-smlua_audio_utils_replace_sequence(6, 16, 75, "Super Mario 3D Land - World 7 (SM64Land)")
-smlua_audio_utils_replace_sequence(8, 12, 75, "Kalamari Desert - Mario Kart 64 (Final) 12")
-smlua_audio_utils_replace_sequence(78, 26, 75, "2-2_Piranha_Creeper_Creek")
-smlua_audio_utils_replace_sequence(36, 37, 75, "0x41 -sewer song Ninst37")
-smlua_audio_utils_replace_sequence(35, 23, 75, "0x38 giant desk theme Ninst23")
-smlua_audio_utils_replace_sequence(51, 26, 75, "3-4outside")
-smlua_audio_utils_replace_sequence(54, 16, 75, "OW4_Retro_Remix2")
-smlua_audio_utils_replace_sequence(40, 17, 75, "Super Mario World - Athletic Remix (NInst 17)")
-smlua_audio_utils_replace_sequence(0x05, 19, 75, "Dire, Dire Docks")
-smlua_audio_utils_replace_sequence(68, 24, 75, "1-4_Yoshi_Star")
-smlua_audio_utils_replace_sequence(44, 37, 75, "Super Mario 64 Slide Remix Ninst 37")
-smlua_audio_utils_replace_sequence(47, 26, 75, "6-1(New Perc)")
-smlua_audio_utils_replace_sequence(67, 26, 75, "OW8_Special (1)")
-smlua_audio_utils_replace_sequence(49, 26, 75, "7-2MM5GravityBEEP2")
-smlua_audio_utils_replace_sequence(50, 26, 75, "7-4(SMRPG Weapons)")
-smlua_audio_utils_replace_sequence(38, 28, 75, "Long note for audio playback_lofidkc")
-smlua_audio_utils_replace_sequence(43, 37, 75, "0x09 Cloud Cap (Ver 2) NInst 37")
-smlua_audio_utils_replace_sequence(69, 17, 75, "Airship Fortress - Mario Kart DS (SM3DL) v2")
-smlua_audio_utils_replace_sequence(70, 26, 75, "SpaceJunk_Andy_NI26")
-smlua_audio_utils_replace_sequence(55, 26, 75, "8-1_Good_egg")
+smlua_audio_utils_replace_sequence(SEQ_LEVEL_GRASS, 37, 75, "SM3DWZeroVv2") --1-1 done
+smlua_audio_utils_replace_sequence(72, 42, 75, "1-2_Puzzle_Plank") --1-2 done
+smlua_audio_utils_replace_sequence(37, 26, 75, "Space1") --1-3 done
+smlua_audio_utils_replace_sequence(71, 42, 75, "SSBTargetTest_Andy_NI26") --Tutorial done
+smlua_audio_utils_replace_sequence(6, 42, 75, "Super Mario 3D Land - World 7 (SM64Land)") --World 2 done
+smlua_audio_utils_replace_sequence(8, 42, 75, "Kalamari Desert - Mario Kart 64 (Final) 12") --2-1 done
+smlua_audio_utils_replace_sequence(78, 42, 75, "2-2_Piranha_Creeper_Creek") --2-2 done
+smlua_audio_utils_replace_sequence(36, 37, 75, "0x41 -sewer song Ninst37") --3-1 done
+smlua_audio_utils_replace_sequence(35, 23, 75, "0x38 giant desk theme Ninst23") --3-3 done
+smlua_audio_utils_replace_sequence(51, 42, 75, "3-4outside") --3-4 done
+smlua_audio_utils_replace_sequence(54, 42, 75, "OW4_Retro_Remix2") --World 4 done
+smlua_audio_utils_replace_sequence(40, 17, 75, "Super Mario World - Athletic Remix (NInst 17)") --4-4 done
+smlua_audio_utils_replace_sequence(5, 19, 75, "Dire, Dire Docks") --3-2 done
+smlua_audio_utils_replace_sequence(68, 42, 45, "1-4_Yoshi_Star") --1-4 done
+smlua_audio_utils_replace_sequence(44, 37, 75, "Super Mario 64 Slide Remix Ninst 37") --6-4 done
+smlua_audio_utils_replace_sequence(47, 42, 75, "6-1(New Perc)3") --6-1 done
+smlua_audio_utils_replace_sequence(67, 42, 75, "OW8_Special (1)") --World 6 done
+smlua_audio_utils_replace_sequence(49, 42, 75, "7-2MM5GravityBEEP2") --7-2 done
+smlua_audio_utils_replace_sequence(50, 42, 75, "7-4(SMRPG Weapons)") --7-3 done
+smlua_audio_utils_replace_sequence(38, 28, 75, "Long note for audio playback_lofidkc") --5-3 use streamed music for this
+smlua_audio_utils_replace_sequence(43, 37, 75, "0x09 Cloud Cap (Ver 2) NInst 37") --6-2 done
+smlua_audio_utils_replace_sequence(69, 42, 75, "Airship Fortress - Mario Kart DS (SM3DL) v2") --7-4 done
+smlua_audio_utils_replace_sequence(70, 37, 75, "SpaceJunk_Andy_NI26") --World 8 done
+smlua_audio_utils_replace_sequence(55, 42, 75, "8-1_Good_egg") --8-1 done
 
 --RM2C
-smlua_audio_utils_replace_sequence(0x01, 0x11, 75, "01_Seq_custom")
-smlua_audio_utils_replace_sequence(0x02, 0x25, 75, "02_Seq_custom")
-smlua_audio_utils_replace_sequence(0x2E, 19, 75, "2E_Seq_custom")
-smlua_audio_utils_replace_sequence(0x04, 0x15, 75, "04_Seq_custom")
-smlua_audio_utils_replace_sequence(0x0B, 0x14, 75, "0B_Seq_custom")
-smlua_audio_utils_replace_sequence(0x0D, 0x16, 75, "0D_Seq_custom")
-smlua_audio_utils_replace_sequence(0x0E, 0x17, 75, "0E_Seq_custom")
-smlua_audio_utils_replace_sequence(0x0F, 0x18, 75, "0F_Seq_custom")
-smlua_audio_utils_replace_sequence(59, 37, 75, "3B_Seq_custom")
-smlua_audio_utils_replace_sequence(0x10, 0x12, 75, "10_Seq_custom")
-smlua_audio_utils_replace_sequence(0x12, 0x11, 75, "12_Seq_custom")
-smlua_audio_utils_replace_sequence(0x14, 0x1A, 75, "14_Seq_custom")
-smlua_audio_utils_replace_sequence(0x15, 0x0E, 75, "15_Seq_custom")
-smlua_audio_utils_replace_sequence(0x16, 0x15, 75, "16_Seq_custom")
-smlua_audio_utils_replace_sequence(0x17, 0x1A, 75, "17_Seq_custom")
-smlua_audio_utils_replace_sequence(0x19, 0x10, 75, "19_Seq_custom")
-smlua_audio_utils_replace_sequence(0x1B, 0x14, 75, "1B_Seq_custom")
-smlua_audio_utils_replace_sequence(0x1C, 0x20, 75, "1C_Seq_custom")
-smlua_audio_utils_replace_sequence(0x1D, 0x1E, 75, "1D_Seq_custom")
-smlua_audio_utils_replace_sequence(0x1E, 0x1B, 75, "1E_Seq_custom")
-smlua_audio_utils_replace_sequence(0x1F, 0x1A, 75, "1F_Seq_custom")
-smlua_audio_utils_replace_sequence(0x20, 0x23, 75, "20_Seq_custom")
-smlua_audio_utils_replace_sequence(0x21, 0x24, 75, "21_Seq_custom")
-smlua_audio_utils_replace_sequence(0x42, 0x0B, 75, "42_Seq_custom")
-smlua_audio_utils_replace_sequence(0x4C, 0x1A, 75, "4C_Seq_custom")
-smlua_audio_utils_replace_sequence(0x4B, 0x1A, 75, "4B_Seq_custom")
-smlua_audio_utils_replace_sequence(0x51, 0x11, 75, "51_Seq_custom")
-smlua_audio_utils_replace_sequence(0x52, 0x1C, 75, "52_Seq_custom")
-smlua_audio_utils_replace_sequence(0x0C, 0x10, 75, "0C_Seq_custom")
+smlua_audio_utils_replace_sequence(0x01, 0x11, 75, "01_Seq_custom") --Star Collect
+smlua_audio_utils_replace_sequence(0x02, 0x25, 75, "02_Seq_custom") --Title Screen
+smlua_audio_utils_replace_sequence(0x2E, 0x2A, 75, "2E_Seq_custom") --4-3 done
+smlua_audio_utils_replace_sequence(0x04, 0x2A, 75, "04_Seq_custom") --World 1 done
+smlua_audio_utils_replace_sequence(0x0B, 0x14, 75, "0B_Seq_custom") --piranha plant
+smlua_audio_utils_replace_sequence(0x0D, 0x16, 75, "0D_Seq_custom") --star select
+smlua_audio_utils_replace_sequence(0x0E, 0x17, 75, "0E_Seq_custom") --powerful mario
+smlua_audio_utils_replace_sequence(0x0F, 0x18, 75, "0F_Seq_custom") --metallic mario
+smlua_audio_utils_replace_sequence(0x3B, 0x25, 75, "3B_Seq_custom") --Toad House
+smlua_audio_utils_replace_sequence(0x10, 0x12, 75, "10_Seq_custom") --koopa message
+smlua_audio_utils_replace_sequence(0x12, 0x11, 75, "12_Seq_custom") --Star Collect
+smlua_audio_utils_replace_sequence(0x14, 0x1A, 75, "14_Seq_custom") --race fanfare
+smlua_audio_utils_replace_sequence(0x15, 0x0E, 75, "15_Seq_custom") --star spawn
+smlua_audio_utils_replace_sequence(0x16, 0x15, 75, "16_Seq_custom") --boss
+smlua_audio_utils_replace_sequence(0x17, 0x1A, 75, "17_Seq_custom") --boss win
+smlua_audio_utils_replace_sequence(0x19, 0x2A, 75, "19_Seq_custom") --World 3 done
+smlua_audio_utils_replace_sequence(0x1B, 0x14, 75, "1B_Seq_custom") --secret
+smlua_audio_utils_replace_sequence(0x1C, 0x20, 75, "1C_Seq_custom") --toad message
+smlua_audio_utils_replace_sequence(0x1D, 0x1E, 75, "1D_Seq_custom") --peach message
+smlua_audio_utils_replace_sequence(0x1E, 0x1B, 75, "1E_Seq_custom") --intro cutscene
+smlua_audio_utils_replace_sequence(0x1F, 0x1A, 75, "1F_Seq_custom") --final boss beat
+smlua_audio_utils_replace_sequence(0x20, 0x23, 75, "20_Seq_custom") --final peach cutscene
+smlua_audio_utils_replace_sequence(0x21, 0x24, 75, "21_Seq_custom") --file select
+smlua_audio_utils_replace_sequence(0x42, 0x0B, 75, "42_Seq_custom") --long note
+smlua_audio_utils_replace_sequence(0x4C, 0x2A, 75, "4C_Seq_custom") --World 5 done
+smlua_audio_utils_replace_sequence(0x4B, 0x2A, 75, "4B_Seq_custom") --2-4 done
+smlua_audio_utils_replace_sequence(0x51, 0x2A, 75, "51_Seq_custom") --World7 done
+smlua_audio_utils_replace_sequence(0x52, 0x1C, 75, "52_Seq_custom") --long note 2
+smlua_audio_utils_replace_sequence(0x0C, 0x2A, 75, "0C_Seq_custom") --2-3 done
