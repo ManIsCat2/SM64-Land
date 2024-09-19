bf039a553b7129833b901f1b2a6cf01b40e92372
ManIsCat2 <137772623+ManIsCat2@users.noreply.github.com>
2024-09-19 09:29:18 +0330

    export bitfs

---
+++ levels/bitfs/textureNew.inc.c
@@ -0,0 +1,246 @@
+ALIGNED8 u8 bitfs_1__texture_0E000018[] = {
+#include "levels/bitfs/bitfs_1_0xe000018_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E001020[] = {
+#include "levels/bitfs/bitfs_1_0xe001020_custom.i8.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E00EC20[] = {
+#include "levels/bitfs/bitfs_1_0xe00ec20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E005C20[] = {
+#include "levels/bitfs/bitfs_1_0xe005c20_custom.i8.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E006C20[] = {
+#include "levels/bitfs/bitfs_1_0xe006c20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E00E420[] = {
+#include "levels/bitfs/bitfs_1_0xe00e420_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E00F420[] = {
+#include "levels/bitfs/bitfs_1_0xe00f420_custom.i8.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E005820[] = {
+#include "levels/bitfs/bitfs_1_0xe005820_custom.i8.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E007C20[] = {
+#include "levels/bitfs/bitfs_1_0xe007c20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E008420[] = {
+#include "levels/bitfs/bitfs_1_0xe008420_custom.i8.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E012220[] = {
+#include "levels/bitfs/bitfs_1_0xe012220_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E006820[] = {
+#include "levels/bitfs/bitfs_1_0xe006820_custom.i8.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E00A820[] = {
+#include "levels/bitfs/bitfs_1_0xe00a820_custom.i8.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E00A020[] = {
+#include "levels/bitfs/bitfs_1_0xe00a020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E010620[] = {
+#include "levels/bitfs/bitfs_1_0xe010620_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E014220[] = {
+#include "levels/bitfs/bitfs_1_0xe014220_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E007820[] = {
+#include "levels/bitfs/bitfs_1_0xe007820_custom.i8.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E008820[] = {
+#include "levels/bitfs/bitfs_1_0xe008820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E007420[] = {
+#include "levels/bitfs/bitfs_1_0xe007420_custom.i8.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E006020[] = {
+#include "levels/bitfs/bitfs_1_0xe006020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E00FE20[] = {
+#include "levels/bitfs/bitfs_1_0xe00fe20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E011220[] = {
+#include "levels/bitfs/bitfs_1_0xe011220_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E009020[] = {
+#include "levels/bitfs/bitfs_1_0xe009020_custom.i8.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E009820[] = {
+#include "levels/bitfs/bitfs_1_0xe009820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E012A20[] = {
+#include "levels/bitfs/bitfs_1_0xe012a20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E013220[] = {
+#include "levels/bitfs/bitfs_1_0xe013220_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E017240[] = {
+#include "levels/bitfs/bitfs_1_0xe017240_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E014A20[] = {
+#include "levels/bitfs/bitfs_1_0xe014a20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E016A40[] = {
+#include "levels/bitfs/bitfs_1_0xe016a40_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E015220[] = {
+#include "levels/bitfs/bitfs_1_0xe015220_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E001420[] = {
+#include "levels/bitfs/bitfs_1_0xe001420_custom.i8.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E002420[] = {
+#include "levels/bitfs/bitfs_1_0xe002420_custom.i8.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E003420[] = {
+#include "levels/bitfs/bitfs_1_0xe003420_custom.i8.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E004420[] = {
+#include "levels/bitfs/bitfs_1_0xe004420_custom.i8.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E005420[] = {
+#include "levels/bitfs/bitfs_1_0xe005420_custom.i8.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E00AC20[] = {
+#include "levels/bitfs/bitfs_1_0xe00ac20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E00BC20[] = {
+#include "levels/bitfs/bitfs_1_0xe00bc20_custom.i8.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E00C020[] = {
+#include "levels/bitfs/bitfs_1_0xe00c020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E00C820[] = {
+#include "levels/bitfs/bitfs_1_0xe00c820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E00D020[] = {
+#include "levels/bitfs/bitfs_1_0xe00d020_custom.i8.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E00D820[] = {
+#include "levels/bitfs/bitfs_1_0xe00d820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E00E020[] = {
+#include "levels/bitfs/bitfs_1_0xe00e020_custom.i8.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E00F620[] = {
+#include "levels/bitfs/bitfs_1_0xe00f620_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E010E20[] = {
+#include "levels/bitfs/bitfs_1_0xe010e20_custom.ia8.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E016220[] = {
+#include "levels/bitfs/bitfs_1_0xe016220_custom.ci4.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E016A20[] = {
+#include "levels/bitfs/bitfs_1_0xe016220_custom.ci4.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E017A40[] = {
+#include "levels/bitfs/bitfs_1_0xe017a40_custom.ia8.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0E00D420[] = {
+#include "levels/bitfs/bitfs_1_0xe00d420_custom.ia8.inc.c"
+};
+ALIGNED8 u8 bitfs_1__texture_0700F018[] = {
+#include "levels/bitfs/bitfs_1_0x700f018_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_2__texture_0E008020[] = {
+#include "levels/bitfs/bitfs_2_0xe008020_custom.ci4.inc.c"
+};
+ALIGNED8 u8 bitfs_2__texture_0E008820[] = {
+#include "levels/bitfs/bitfs_2_0xe008020_custom.ci4.inc.c"
+};
+ALIGNED8 u8 bitfs_2__texture_0E004C20[] = {
+#include "levels/bitfs/bitfs_2_0xe004c20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_2__texture_0E009040[] = {
+#include "levels/bitfs/bitfs_2_0xe009040_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_2__texture_0E005C20[] = {
+#include "levels/bitfs/bitfs_2_0xe005c20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_2__texture_0E000018[] = {
+#include "levels/bitfs/bitfs_2_0xe000018_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_2__texture_0E001020[] = {
+#include "levels/bitfs/bitfs_2_0xe001020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_2__texture_0E000820[] = {
+#include "levels/bitfs/bitfs_2_0xe000820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_2__texture_0E003C20[] = {
+#include "levels/bitfs/bitfs_2_0xe003c20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_2__texture_0E007820[] = {
+#include "levels/bitfs/bitfs_2_0xe007820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_2__texture_0E006420[] = {
+#include "levels/bitfs/bitfs_2_0xe006420_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_2__texture_0E007020[] = {
+#include "levels/bitfs/bitfs_2_0xe007020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_2__texture_0E005420[] = {
+#include "levels/bitfs/bitfs_2_0xe005420_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_2__texture_0E006C20[] = {
+#include "levels/bitfs/bitfs_2_0xe006c20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_2__texture_0E008840[] = {
+#include "levels/bitfs/bitfs_2_0xe008840_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_2__texture_0E001C20[] = {
+#include "levels/bitfs/bitfs_2_0xe001c20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_2__texture_0E002C20[] = {
+#include "levels/bitfs/bitfs_2_0xe002c20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_2__texture_0E001420[] = {
+#include "levels/bitfs/bitfs_2_0xe001420_custom.ia8.inc.c"
+};
+ALIGNED8 u8 bitfs_2__texture_0E001820[] = {
+#include "levels/bitfs/bitfs_2_0xe001820_custom.ia8.inc.c"
+};
+ALIGNED8 u8 bitfs_3__texture_0E000018[] = {
+#include "levels/bitfs/bitfs_3_0xe000018_custom.i8.inc.c"
+};
+ALIGNED8 u8 bitfs_3__texture_0E005420[] = {
+#include "levels/bitfs/bitfs_3_0xe005420_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_3__texture_0E002820[] = {
+#include "levels/bitfs/bitfs_3_0xe002820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_3__texture_0E004820[] = {
+#include "levels/bitfs/bitfs_3_0xe004820_custom.i8.inc.c"
+};
+ALIGNED8 u8 bitfs_3__texture_0E005C20[] = {
+#include "levels/bitfs/bitfs_3_0xe005c20_custom.i8.inc.c"
+};
+ALIGNED8 u8 bitfs_3__texture_0E004020[] = {
+#include "levels/bitfs/bitfs_3_0xe004020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_3__texture_0E006420[] = {
+#include "levels/bitfs/bitfs_3_0xe006420_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_3__texture_0E001020[] = {
+#include "levels/bitfs/bitfs_3_0xe001020_custom.i8.inc.c"
+};
+ALIGNED8 u8 bitfs_3__texture_0E004C20[] = {
+#include "levels/bitfs/bitfs_3_0xe004c20_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_3__texture_0E002020[] = {
+#include "levels/bitfs/bitfs_3_0xe002020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_3__texture_0E003020[] = {
+#include "levels/bitfs/bitfs_3_0xe003020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_3__texture_0E003820[] = {
+#include "levels/bitfs/bitfs_3_0xe003820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 bitfs_3__texture_0E005820[] = {
+#include "levels/bitfs/bitfs_3_0xe005820_custom.ia8.inc.c"
+};
+ALIGNED8 u8 bitfs_3__texture_0E006C20[] = {
+#include "levels/bitfs/bitfs_3_0xe006c20_custom.ia8.inc.c"
+};
