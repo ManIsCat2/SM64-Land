64256e2f7c7e454de5e8208abce9a2f247c2766d
ManIsCat2 <137772623+ManIsCat2@users.noreply.github.com>
2024-09-19 18:40:15 +0330

    rely lll on rm2c

---
+++ levels/lll/areas/1/movtextNew.inc.c
@@ -0,0 +1,12 @@
+static Movtex lll_1_Movtex_0_0[] = {1, 0, 15, 16, -32000, -32000, 32000, -32000, 32000, 32000, -32000, 32000, 1, 79, 3, 0};
+
+const struct MovtexQuadCollection lll_1_Movtex_0[] = {
+{0,lll_1_Movtex_0_0},
+{-1, NULL},
+};
+const struct MovtexQuadCollection lll_1_Movtex_1[] = {
+{-1, NULL},
+};
+const struct MovtexQuadCollection lll_1_Movtex_2[] = {
+{-1, NULL},
+};
